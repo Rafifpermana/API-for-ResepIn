09e1ac3d5a1c86e88b8d404da155f017e1781c7c
DRIV32 <rafifpermana39@gmail.com>
2025-05-12 21:29:29 +0700

    fix: update version compatibility7

--- loader.py
+++ loader.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import joblib
-from scipy.sparse import load_npz
+
 
 # Load model dan data dengan format terkompresi
 tfidf = joblib.load("models/tfidf_model.pkl")  # Model TF-IDF terkompresi
-tfidf_matrix = load_npz("models/tfidf_matrix.npz")  # Format sparse matrix
-data = pd.read_parquet("models/data_resep_bersih.parquet")  # Format Parquet
+tfidf_matrix = joblib.load("models/tfidf_matrix.pkl")  # Changed to load with joblib
+data = pd.read_csv("models/data_resep_bersih.csv")  # Changed to read CSV
 
 # Normalisasi kolom kategori
 data['kategori_bahan'] = data['kategori_bahan'].str.strip().str.lower()
